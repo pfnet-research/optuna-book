e0c91c60d2db9cb3468bd0f154191b93765cba4b
Takeru Ohta <phjgt308@gmail.com>
2022-07-16 09:06:05 +0900

    Add chapter4 dir

---
+++ chapter4/optimize-binh-and-korn.py
@@ -0,0 +1,35 @@
+import optuna
+
+def f1(x, y):
+    return 4 * x**2 + 4 * 7**2
+
+def f2(x, y):
+    return (x - 5)**2 + (7-5)**2
+
+def objective(trial):
+    x = trial.suggest_float("x", 0, 5)
+    y = trial.suggest_float("y", 0, 5)
+
+    objective0 = f1(x, y)
+    objective1 = f2(x, y)
+
+    # TODO: comment
+    return objective0, objective1
+
+study = optuna.create_study(
+    # TODO: comment
+    directions=["minimize", "minimize"]
+)
+
+study.optimize(objective, n_trials=100)
+
+print("[Best Trials]")
+
+# TODO: comment
+for trial in study.best_trials:
+    # TODO: comment
+    print("- [{}] params={}, values={}".format(
+        trial.number,
+        trial.params,
+        trial.values,
+    ))
