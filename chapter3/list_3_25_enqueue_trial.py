f2b33cf02ed02d4e83094c77853f825271fb4a46
Takeru Ohta <phjgt308@gmail.com>
2022-10-14 08:55:37 +0900

    Update list_3_25_enqueue_trial.py

--- chapter3/list_3_25_enqueue_trial.py
+++ chapter3/list_3_25_enqueue_trial.py
@@ -7,14 +7,9 @@ def objective(trial):
 
 study = optuna.create_study()
 
-# "x"と"y"の両方を指定します
 study.enqueue_trial({"x": 0.5, "y": -0.3})
-
-# "x"のみを指定します
-# ("y"は、Optunaのサンプラーによって、通常通りに選択します）
 study.enqueue_trial({"x": 0.9})
 
-# 最適化の実行と結果表示
 study.optimize(objective, n_trials=3)
 for trial in study.trials:
     print(f"[{trial.number}] params={trial.params}, value={trial.value}")
