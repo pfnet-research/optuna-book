e2a91a07ada89e72692e14974650187210046a55
Hideaki Imamura <hideakiimamura@mamu.lan>
2022-11-11 15:42:37 +0900

    Add a code for section 3.6

---
+++ chapter3/list_3_36_change_sampler_when_create_study.py
@@ -0,0 +1,15 @@
+import optuna
+
+
+def objective(trial):
+    x = trial.suggest_float("x", -4.5, 4.5)
+    y = trial.suggest_float("y", -4.5, 4.5)
+    return (1.5 - x + x * y) ** 2 + \
+        (2.25 - x + x * y ** 2) ** 2 + \
+        (2.625 - x + x * y ** 3) ** 2
+
+
+study = optuna.create_study(sampler=optuna.samplers.CmaEsSampler())
+study.optimize(objective, n_trials=5000)
+print(f"Best objective value: {study.best_value}")
+print(f"Best parameter: {study.best_params}")
